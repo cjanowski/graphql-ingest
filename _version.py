be30f1335068752410210147c4b52aeb6e6eea6c
Cory Janowski <coryjanowski@gmail.com>
2025-05-25 20:09:41 -0400

    Version update

--- _version.py
+++ _version.py
@@ -17,5 +17,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = '0.1.1.dev0+ga153c48.d20250525'
-__version_tuple__ = version_tuple = (0, 1, 1, 'dev0', 'ga153c48.d20250525')
+__version__ = version = '0.1.1.dev19+g4d55fb5.d20250526'
+__version_tuple__ = version_tuple = (0, 1, 1, 'dev19', 'g4d55fb5.d20250526')
