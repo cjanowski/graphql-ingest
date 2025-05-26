73277e9eaeadbbdf8e8745de7f23cd17d409f544
Cory Janowski <coryjanowski@gmail.com>
2025-05-25 21:01:32 -0400

    Spacing fix, toml update

--- _version.py
+++ _version.py
@@ -17,5 +17,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = '0.1.1.dev19+g4d55fb5.d20250526'
-__version_tuple__ = version_tuple = (0, 1, 1, 'dev19', 'g4d55fb5.d20250526')
+__version__ = version = '0.1.1.dev21+g629366c.d20250526'
+__version_tuple__ = version_tuple = (0, 1, 1, 'dev21', 'g629366c.d20250526')
